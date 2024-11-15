f3848a97c81123c3a9413db1e9a6044fcb0f3d8a
sromero1905 <santirom1905@gmail.com>
2024-11-15 19:52:55 -0300

    Agregar archivos de proyecto.

---
+++ CapaEntidad/Carrito.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    internal class Carrito
+    {
+        public int IdCarrito { get; set; }
+        public Cliente oCliente { get; set; }
+        public Producto oProducto { get; set; }
+        public int Cantidad { get; set; }
+    }
+}
---
+++ CapaPresentacionAdmin/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CapaEntidad;
+using CapaNegocio;
+
+
+
+
+namespace CapaPresentacionAdmin.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Usuarios()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult ListarUsuarios()
+        {
+            List<Usuario> oLista = new List<Usuario>();
+
+            oLista = new CN_Usuarios().listar();
+
+            return Json(new {data = oLista }, JsonRequestBehavior.AllowGet);
+
+        }
+    }
+}
